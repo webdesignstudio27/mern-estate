9b4e06eb167e88476813d7748cec393bef8ada8f
webdesignstudio27 <sivasuvedha@gmail.com>
2025-02-28 15:39:37 +0530

    create user model

--- api/index.js
+++ api/index.js
@@ -14,7 +14,7 @@ mongoose
 
 const app = express();
 
-app.listen(5000, () =>{
-    console.log('Server is running on port 5000!');
+app.listen(3000, () =>{
+    console.log('Server is running on port 3000!');
     }
 )
