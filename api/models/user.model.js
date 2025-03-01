8c347bb98637081baa2ae980ed5b7e194ea597a6
webdesignstudio27 <sivasuvedha@gmail.com>
2025-03-01 12:55:25 +0530

    create sign up api route

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,22 +1,21 @@
 import mongoose from 'mongoose';
 
-const userSchema = new mongoose.Aggregate.Schema({
-    username:{
-        type:String,
-        required:true,
-        unique:true,
+const userSchema = new mongoose.Schema({  
+    username: {
+        type: String,
+        required: true,
+        unique: true,
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true,
+    email: {
+        type: String,
+        required: true,
+        unique: true,
     },
-    password:{
-        type:String,
-        required:true,
-       
+    password: {
+        type: String,
+        required: true,
     },
-},{ timestamps: true });
+}, { timestamps: true });
 
 const User = mongoose.model('User', userSchema);
 
